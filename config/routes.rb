4c52d43c9559606f7724d9d0bb162e7b01bafc87
kangduan <duanea0606@gmail.com>
2017-02-13 10:19:29 +0000

    dfjk

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
     resources :messages, only: [:create]
   end
   devise_for :users
-  get 'home/index'
-  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
